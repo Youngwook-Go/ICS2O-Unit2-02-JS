c2a3449d10825f8d2c6d89c550c74e61bcd29c9b
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-06 18:31:51 +0000

    a

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Youngwook All rights reserved
+//
+// Created by: Youngwook
+// Created on: Oct 2022
+// This file contains the JS functions for index1.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() 
+{
+  document.getElementById("add-math").innerHTML = "<p> 6 + 5 = " + (6 + 5) + "</p>"
+  document.getElementById("subtract-math").innerHTML = "<p> 7 - 3 =  " + (7 - 3) + "</p>"  
+  document.getElementById("multiple-math").innerHTML = "<p> 3 + 4 × 2 = " + (3 + 4 * 2) + "</p>" 
+  document.getElementById("divide-math").innerHTML = "<p> (4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
+  document.getElementById("exponet-math").innerHTML = "<p> 5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+}
